b88f4c681414c3e2fdcdcf01acdfe13bba766b2c
Jose <josemiguelortegagaviria@gmail.com>
2025-08-20 21:12:21 -0500

    The class has been created

--- 03-data-structures.js
+++ 03-data-structures.js
@@ -26,149 +26,50 @@ console.log(`
 `);
 
 class Contact {
+    #id;
+    #name;
+    #phoneNumber;
     constructor(name, phoneNumber){
-        this.name = name;
-        this.phoneNumber = phoneNumber;
+        this.#id = ++Contact.idAssignation;
+        this.#name = name;
+        this.#phoneNumber = this.#verifierPhoneNumbers(phoneNumber);
+    };
+
+    // static properties
+    static idAssignation = 0;
+
+    // getters and setters
+    get id(){
+        return this.#id;
+    };
+    get name(){
+        return this.#name;
+    };
+    get phoneNumber(){
+        return this.#phoneNumber;
+    };
+    set name(newName){
+        this.#name = newName;
+    };
+    set phoneNumber(newPhoneNumber){
+        this.#phoneNumber = newPhoneNumber;
+    };
+
+    // Methods
+    #verifierPhoneNumbers(phoneNumber){
+        if(typeof(phoneNumber) != "number" || phoneNumber.toString().length != 10){
+            throw new Error("There's an error in the number inserted.");
+        }else{
+            return phoneNumber;
+        };
     };
 
 };
 
+// Proof
 
+const contactOne = new Contact("Jose", 3126699609);
+const contactTwo = new Contact("Jerald", 3127498808);
+const contactThree = new Contact("Jacke", 3042553669);
 
-// --------------------------------------------------------------------
-// --------------------------------------------------------------------
-// --------------------------------------------------------------------
-
-
-
-
-
-
-class Person {
-    constructor(name, age, alias) {
-        this.name = name;
-        this.age = age;
-        this.alias = alias;
-    }
-}
-
-let person = new Person("Miguel", 30, "Chepe");
-
-console.log(typeof (person));
-console.log(person);
-
-// -----------------------------------------------------Default values
-
-class Aliens {
-    constructor(name = "Without name", age = 0, alias = "Without alias") {
-        this.name = name;
-        this.age = age;
-        this.alias = alias;
-    }
-}
-
-let alienOne = new Aliens();
-console.log(alienOne);
-
-// -----------------------------------------------------Access to properties
-
-console.log(alienOne.name);
-console.log(alienOne["name"]);
-
-alienOne.name = "Gray";
-console.log(alienOne.name);
-
-// -----------------------------------------------------Methods
-
-class Animals {
-    constructor(name, age) {
-        this.name = name;
-        this.age = age;
-    }
-    walk() {
-        console.log(`The animal called ${this.name} walks`);
-    }
-}
-
-let animalOne = new Animals("Roco", 8);
-animalOne.walk();
-
-// -----------------------------------------------------Properties encapsulation
-
-// Private
-
-class privatePerson {
-    #name;
-    #age;
-    #alias;
-    #bank;
-
-    constructor(name, age, alias, bank) {
-        this.#name = name;
-        this.#age = age;
-        this.#alias = alias;
-        this.#bank = bank;
-    }
-
-    get name() {
-        return this.#name
-    }
-    get bank() {
-        return this.#bank;
-    }
-    set bank(newBank) {
-        this.#bank = newBank;
-    }
-}
-
-let banckPerson = new privatePerson("Jose", 30, "Chepe", "IBAN125456");
-
-banckPerson.bank = "New IBAN12354";
-
-console.log(banckPerson.name);
-console.log(banckPerson.bank);
-
-// -----------------------------------------------------Inheritance
-
-class Animales {
-    constructor(name) {
-        this.name = name;
-    }
-    sound() {
-        console.log(`The animal called ${this.name} emits a sound`);
-    }
-}
-
-class Perros extends Animales {
-
-    constructor(name, age) {
-        super(name);
-        this.age = age;
-    }
-
-    run() {
-        console.log(`The dog ${this.age} old runs`)
-    }
-
-    sound() {
-        console.log(`The dog barks`);
-    }
-}
-
-let myDog = new Perros("Sasha", 8);
-
-myDog.run();
-myDog.sound();
-
-// -----------------------------------------------------Static methods
-// We don't need to create an instance of this type of classes ot methods
-
-class MathOperations {
-
-    static sum(a, b) {
-
-        return a + b;
-    }
-}
-
-console.log(MathOperations.sum(5, 10));
+console.log(contactOne.phoneNumber);
