aa89cc98b37374d0a1ee0e56372cc7c0283c1e70
Jose <josemiguelortegagaviria@gmail.com>
2025-08-22 14:39:04 -0500

    The chagenge has been finished.

--- 03-data-structure/contactListApp.js
+++ 03-data-structure/contactListApp.js
@@ -2,8 +2,8 @@ const options = `
 Hello and welcome to your contact list.
 Choose the option you are looking for:
 
-1 . Search for a contact.
-2 . Add a new contact.
+1 . Add a new contact.
+2 . Search for a contact.
 3 . Update a contact.
 4 . Delete a contact.
 5 . List elements
@@ -35,28 +35,47 @@ async function runApp() {
     
     while(optionSelected != "0" ){
 
-        switch(optionSelected){      
+        switch(optionSelected){  
+
             case "1":
-                const searchedName = await ask("Enter the name: ");
-                console.log(searchContact(contactList,searchedName));
-                break;
-            case "2":
                 const newName = await ask("Enter the name: ");
-                const newPhoneNumber = await ask("Enter their phone number: ");
-                addNewContact(Contact,contactList,newName,newPhoneNumber);
-                console.log(`You've added to ${newName} : ${newPhoneNumber}`);
+                const newPhoneNumber = Number(await ask("Enter their phone number: "));
+                try{
+                    addNewContact(Contact,contactList,newName,newPhoneNumber);
+                    console.log(`You've added to ${newName} : ${newPhoneNumber}`);
+                }catch(error){
+                    console.log(error.message);
+                }
                 break;
+
+            case "2":
+                const searchedName = await ask("Enter the name: ");
+                searchContact(contactList,searchedName);
+                break;
+
             case "3":
                 const searchName = await ask("Enter the name of the contact to update: ");
-                const updatedName = await ask("Enter the new name: ");
-                const updatedPhoneNumber = await ask("Enter the new phone number: ");
-                console.log(updateContact(contactList,searchName,updatedName,updatedPhoneNumber));
+                let contact = contactList.find(element => element.name === searchName);
+                if(contact != undefined){
+                    const updatedName = await ask("Enter the new name: ");
+                    const updatedPhoneNumber = await ask("Enter the new phone number: ");
+                    console.log(updateContact(contactList,searchName,updatedName,updatedPhoneNumber));
+                }else{
+                    console.log(`Contact not found`);
+                };
                 break;
+
             case "4":
                 const deletedName = await ask("Enter the name of the contact to delete: ");
-                contactList = deleteContact(contactList, deletedName);
-                console.log(`You've deleted ${deletedName}`);
+                let dcontact = contactList.find(element => element.name === deletedName);
+                if(dcontact != undefined){
+                    contactList = deleteContact(contactList, deletedName);
+                    console.log(`You've deleted ${deletedName}`);
+                }else{
+                    console.log(`Contact not found`);
+                };
                 break;
+
             case "5":
                 for(let value of contactList){
                     console.log(`Name : ${value.name} - Number : ${value.phoneNumber}`);
--- 03-data-structure/functions.js
+++ 03-data-structure/functions.js
@@ -1,11 +1,22 @@
 export function addNewContact(classRef, array, name, phone){
-    const newContact = new classRef(name, phone);
-    array.push(newContact);
+    if(name && phone && typeof(phone) == "number" && String(phone).length == 10){
+        const newContact = new classRef(name, phone);
+        array.push(newContact);
+    }else{
+        throw new Error("Invalid entry, try again...");
+    };
 };
 
 export function searchContact(array, name){
-    let contact = array.find(element => element.name === name);
-    return `Name: ${contact.name} - Number: ${contact.phoneNumber}`;
+    const filterArray = array.filter(item => item.name == name);
+    if(filterArray.length !== 0){
+        for(let value of filterArray){
+            console.log(`Name : ${value.name} - Number : ${value.phoneNumber}`);
+        };
+    }else{
+        console.log("Contact not found");
+    };
+
 };
 
 export function updateContact(array, name, newName, newPhone){
@@ -18,4 +29,3 @@ export function updateContact(array, name, newName, newPhone){
 export function deleteContact(array, name){
     return array.filter(item => item.name !== name);
 };
-
