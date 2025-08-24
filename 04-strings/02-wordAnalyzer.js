7ee81216adca9bd5959dfbc7bea0f7647cb704aa
Jose <josemiguelortegagaviria@gmail.com>
2025-08-24 16:10:15 -0500

    string challenge has already done

--- 04-strings/02-wordAnalyzer.js
+++ 04-strings/02-wordAnalyzer.js
@@ -1,71 +1,64 @@
+class NumberCounter {
+    constructor(numb, value){
+        this.numb = numb;
+        this.value = value;
+    };
+    increaseCounter(){
+        this.value +=  1;
+    }
+}
+
+function isAnagram(wordOne, wordTwo){
+    let mult = 1;
+    for(let value of wordOne){
+        if(wordTwo.includes(value)){
+            mult *= 1;
+        }else{
+            mult *= 0;
+        };
+    };
+    return mult;
+};
+
+function isIsogram(wordOne, wordTwo){
+    const wordComplete = wordOne + wordTwo;
+    let countArray = [];
+    for(let i = 0; i < wordComplete.length; i++){
+        for(let j = 0; j < wordComplete.length; j++){
+            if(wordComplete[i] === wordComplete[j]){
+                let doesThisValueExist = countArray.find(element => element.numb === wordComplete[i]);
+                if(doesThisValueExist == undefined){
+                    let newNum = new NumberCounter(wordComplete[i],1);
+                    countArray.push(newNum);
+                }else{
+                    let letterCount = countArray.find(element => element.numb === wordComplete[j]);
+                    letterCount.increaseCounter();
+                };
+            };
+        };
+    };
+    const baseValue = countArray[0].value;
+    let result = 1;
+    for(value of countArray){
+        if(value.value === baseValue){
+            result *= 1;
+        }else{
+            result *= 0;
+        };
+    };
+    return result;
+};
+
 function wordAnalyser(wordOne, wordTwo){
     if(wordOne === wordTwo){
         return `The words are a palindrome`
     }else if(isAnagram(wordOne,wordTwo) === 1){
-        return `The words are anagrams`
-    };
-        
-};
-
-let newWord = "";
-function palindrome(word){
-    for(let i = word.length - 1; i >= 0 ; i--){
-        newWord += word[i];
-    };
-    if(word === newWord){
-        return true;
+        return `The words are an anagrams`
+    }else if(isIsogram(wordOne, wordTwo) === 1){
+        return `The words are an isograms`;
     }else{
-        return false;
-        
-    };
+        return `The words aren't a shit`;
+    };      
 };
 
-function isAnagram(wordOne, wordTwo){
-    let mult;
-    for(let value of wordOne){
-        if(wordTwo.includes(value)){
-            return mult *= 1;
-        }else{
-            return mult *= -1;
-        };
-    };
-};
-
-function isIsogram(){
-
-};
-
-
-
-
-class Contact {
-    #id;
-    #name;
-    #phoneNumber;
-    constructor(name, phoneNumber){
-        this.#id = ++Contact.idAssignation;
-        this.#name = name;
-        this.#phoneNumber = phoneNumber;
-    };
-
-    // static properties
-    static idAssignation = 0;
-
-    // getters and setters
-    get id(){
-        return this.#id;
-    };
-    get name(){
-        return this.#name;
-    };
-    get phoneNumber(){
-        return this.#phoneNumber;
-    };
-    set name(newName){
-        this.#name = newName;
-    };
-    set phoneNumber(newPhoneNumber){
-        this.#phoneNumber = newPhoneNumber;
-    };
-};
-export default Contact;
+console.log(wordAnalyser("soon","s"));
